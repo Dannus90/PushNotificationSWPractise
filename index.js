49e5a92b7375afc3c65bdb8993f3bc9254f0dade
Daniel Persson <persson.daniel.1990@gmail.com>
2020-12-08 21:27:36 +0100

    Add server and setup base for push notifications

--- index.js
+++ index.js
@@ -9,5 +9,34 @@ dotenv.config();
 
 const app = express()
 
+// Body parser middleware
+app.use(bodyParser.json())
+
+// Telling who is sending the push notification
 const publicVapidKey = process.env.PUBLIC_VAPID_KEY
 const privateVapidKey = process.env.PRIVATE_VAPID_KEY
+
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey)
+
+// Subscribe Route
+app.post('/subscribe', (req, res) => {
+    // Get pushSubscription object
+    const subsription = req.body
+
+    // Send 201 - Resource created successfully
+    res.status(201).json({})
+
+    // Create payload
+    const payload = JSON.stringify({ title: 'Push Test'})
+
+    // Pass object into sendNotificiation
+    webpush.sendNotification(subsription, payload).catch(err => {
+        console.log(err)
+    })   
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+})
